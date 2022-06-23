5eb27ea5ef924eb5985dc112c12266f3e26ebe1c
Chand-glitch <55791118+Chand-glitch@users.noreply.github.com>
2022-06-23 12:51:50 +0545

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+	var preloader = document.getElementById("preloader");
+	window.addEventListener("load",function(){
+		preloader.style.display = "none";
+	})
+</script>
